d9c1f62c11c1274fd755c12febce93aed9f7842f
rafaelsant <rafael.ribeiro.santiago@gmail.com>
2020-07-12 15:38:27 -0300

    atualização dos estudos 2

---
+++ Javascript/Game_mata_mosquito/jogo.js
@@ -0,0 +1,87 @@
+//altura e largura da página
+var altura
+var largura
+var vidas = 1
+var tempo = 10
+
+var nivel = window.location.search
+nivel.replace('?','')
+if (nivel === 'normal') {
+	//1500
+} else if (nivel ==== 'dificil') {
+	
+}
+function ajustaTamanhoPalcoJogo(){
+	altura = window.innerHeight
+	largura = window.innerWidth
+	console.log(largura, altura)
+}
+ajustaTamanhoPalcoJogo()
+var cronometro = setInterval(function(){
+	tempo-=1
+	if (tempo<0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		window.location.href = 'vitoria.html'
+	}else{
+		document.getElementById('cronometro').innerHTML = tempo
+		}
+	},1000)
+
+function posicaoRandomica(){
+	//remover o mosquito anterior, caso exista
+	var mortes = 0
+	if (document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+		if (vidas >3) {
+			window.location.href = 'fim_de_jogo.html'
+		}else{
+			document.getElementById('v'+vidas).src="imagens/coracao_vazio.png"
+		}
+		vidas++
+	}
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX,posicaoY)
+
+	//criar o elemento HTML
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = tamanhoAleatorio()+ ' ' + ladoAleatorio()
+	mosquito.style.left = posicaoX+"px"
+	mosquito.style.top = posicaoY+"px"
+	mosquito.style.position = 'absolute'
+	document.body.appendChild(mosquito)
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function(){
+		this.remove()
+	}
+
+}
+//função que define aleatóriamente a posição dos mosquitos
+function tamanhoAleatorio(){
+	var classe = Math.floor(Math.random() * 3)
+
+	switch(classe){
+		case 0: 
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
+	}
+}
+//função que define aleatóriamente o lado dos mosquitos
+function ladoAleatorio(){
+	var lado = Math.floor(Math.random() * 2)
+
+	switch(lado){
+		case 0: 
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
+	}
+}
