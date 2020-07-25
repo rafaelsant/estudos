4d92584097543821913d2ceb6a60110f01497fe7
Rafael Ribeiro Santiago <rafael.ribeiro.santiago@gmail.com>
2020-07-25 10:51:49 -0300

    atualizações nos estudos

--- Javascript/Game_Snake/javascripts/snake.js
+++ Javascript/Game_Snake/javascripts/snake.js
@@ -6,4 +6,26 @@ const box = 32
 
 //carregando imagens
 const ground = new Image()
-ground.src
+ground.src = "img/ground.png"
+
+const food = new Image()
+food.src = "img/food.png"
+
+//carregando arquivos de audio
+
+const dead = new Audio()
+const down = new Audio()
+const eat = new Audio()
+const left = new Audio()
+const right = new Audio()
+const up = new Audio()
+
+dead.src = "audio/dead.mp3"
+down.src = "audio/down.mp3"
+eat.src = "audio/eat.mp3"
+left.src = "audio/left.mp3"
+right.src = "audio/right.mp3"
+up.src = "audio/up.mp3"
+
+//criando a cobra
+
