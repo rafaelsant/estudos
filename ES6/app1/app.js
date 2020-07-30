5ed3c64c3e491fbb815e7c1cd76d74162219fadb
Rafael Ribeiro Santiago <rafael.ribeiro.santiago@gmail.com>
2020-07-29 21:13:53 -0300

    atualizaçao estudos

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -33,6 +33,29 @@ class Bd{
         localStorage.setItem(id,JSON.stringify(d))
         localStorage.setItem('id',id)
     }
+    recuperarTodosRegistros(){
+        let id = localStorage.getItem('id')
+        //recuperar todas as despesas cadastrdas em local storage
+        for(let i=1;i<=id;i++){
+            //recuperar as despesas
+            let despesa = JSON.parse(localStorage.getItem(i))
+            console.log(i,despesa)
+        }
+    }
+}
+function modalSucesso(){
+        $('#modalRegistraDespesa').modal('show')
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!'
+        document.getElementById('modalRegistraDespesaText').className = 'modal-header text-success'
+        document.getElementById('modalRegistraDespesaBody').innerHTML = 'Despesa foi cadastrada com sucesso'
+        document.getElementById('modalRegistraDespesaBtn').className = 'btn btn-success'
+}
+function modalErro(){
+    $('#modalRegistraDespesa').modal('show')
+    document.getElementById('modal_titulo').innerHTML = 'Erro no registro!'
+    document.getElementById('modalRegistraDespesaText').className = 'modal-header text-danger'
+    document.getElementById('modalRegistraDespesaBody').innerHTML = 'Erro no registro, insira dados nos campos'
+    document.getElementById('modalRegistraDespesaBtn').className = 'btn btn-danger'
 }
 let bd = new Bd()
 function cadastrarDespesas(){
@@ -53,10 +76,13 @@ function cadastrarDespesas(){
     )
     if(despesa.validarDados()){
         bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
+        modalSucesso()
     }else{
         //exibindo modal
-        $('#erroGravacao').modal('show')
+        modalErro()
     }
     
+}
+function carregaListaDespesas(){
+    bd.recuperarTodosRegistros()
 }
