6ebe2983f3fac1c2708d1382a62c268a8e898d5a
Rafael Ribeiro Santiago <rafael.ribeiro.santiago@gmail.com>
2020-08-01 19:24:27 -0300

    Inicio dos estudos em php7

---
+++ PHP7/comentarios.php
@@ -0,0 +1,22 @@
+
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso PHP</title>
+	</head>
+	<body>
+    <!--Os comentários de html ficam disponíveis na documentaçao do site-->
+		<?php
+            echo 'Comando echo <br>';
+            //isso é um comentário de uma linha
+            #isso é um comentário de uma linha em estilo shell
+            /*
+                Isso é um
+                comentário de multiplas linhas
+                */ 
+            print "Comando print<hr>";
+            
+		?>
+        
+	</body>
+</html>
---
+++ PHP7/index.php
@@ -0,0 +1,16 @@
+
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		<?php
+			echo 'Utilizando a tag padrão';
+		?>
+		<br>
+		<?= 'Utilizando a tag de impressão'?>
+		<br>
+		<? echo 'Utilizando tag curta'; ?>
+	</body>
+</html>
